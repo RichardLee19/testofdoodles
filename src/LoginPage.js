5f00d6d7e1712a6aa05c878c40437ba82cbd754e
Richard Lee <73311642+RichardLee19@users.noreply.github.com>
2024-02-02 20:21:20 -0800

    s

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 
+
 function LoginPage() {
-    const [isSignIn, setIsSignIn] = useState(true);
+    const [isSignIn, setIsSignIn] = useState(true); 
     const [email, setEmail] = useState('');
 
     const handleToggleMode = () => {
@@ -15,7 +16,16 @@ function LoginPage() {
     return (
         <div className="container">
             <h1>{"Welcome to SportsCar Spotter"}</h1>
-            <h2>{isSignIn ? 'Sign In' : 'Create Account'}</h2>
+            {
+            //Since value is set to True, will default to Sign in. Else, header changes to Create Account.
+            }
+            <h2>{isSignIn ? 'Sign In' : 'Create Account'}</h2> 
+
+            {
+            //Conditional Rendering Statement. If !iSignIn (If we are in the Create Account Page), display
+            //an extra text box to input an email address. 
+            }
+
             { !isSignIn && (
                 <div className="input-group">
                     <input type="email" placeholder="Email" value={email} onChange={handleEmailChange} />
@@ -27,11 +37,15 @@ function LoginPage() {
             <div className="input-group">
                 <input type="password" placeholder="Password" />
             </div>
-            <button className="btn">{isSignIn ? 'Sign In' : 'Create Account'}</button>
-            
             { isSignIn && (
-                <button className="btn">{'Forgot Password?'}</button>
+                <button className="small-btn">{'Forgot Password?'}</button>
             )}
+            <br />
+            {
+            // if isSignIn is True, show 'Sign In'; else show 'Create Account'.
+            }
+            <button className="btn">{isSignIn ? 'Sign In' : 'Create Account'}</button>
+
             <p>{isSignIn ? "Don't have an account? " : "Already have an account? "} 
                <button className="btn" onClick={handleToggleMode}>
                    {isSignIn ? 'Create Account' : 'Sign In'}
